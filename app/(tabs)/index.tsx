daaf250cd7228e416893992e1df9e547107e713c
Am1n <aminghaedi.mail@gmail.com>
2025-08-01 19:28:00 +0330

    add the database using convex | add the queries and the mutations for todos(CRUD methods)

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,16 @@
+import { api } from "@/convex/_generated/api";
 import useTheme from "@/hooks/useTheme";
+import { useMutation, useQuery } from "convex/react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   const {toggleDarkMode} = useTheme();
+
+
+  const addTodo = useMutation(api.todos.addTodo)
+  const clearAllTodos = useMutation(api.todos.clearAllTodos)
+  const todos = useQuery(api.todos.getTodos)
+  console.log()
   return (
     <View
       style={styles.container}
@@ -12,6 +20,16 @@ export default function Index() {
       <TouchableOpacity onPress={toggleDarkMode}>
         <Text>
           toggle the mode
+        </Text>  
+        </TouchableOpacity>
+      <TouchableOpacity onPress={() => addTodo({text: "learn python"})}>
+        <Text>
+          Add a new todo
+        </Text>  
+        </TouchableOpacity>
+    <TouchableOpacity onPress={() => clearAllTodos()}>
+        <Text>
+          Clear All
         </Text>  
         </TouchableOpacity>
     </View>
